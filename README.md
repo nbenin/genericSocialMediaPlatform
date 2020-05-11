1c28cbd37e228295a594b09d9b7bca05d59058a3
Neil <neil.mf.benin@gmail.com>
2020-05-11 16:36:25 +0200

    slowly working on it, learning the artisan commands

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
+# Generic Social Media Platform
+
+Attempting to make a "Social Media" 
+First time using Laravel, mostly experimenting
+
+
+
+
+
+
+
+
+
+
+
 <p align="center"><img src="https://res.cloudinary.com/dtfbvvkyp/image/upload/v1566331377/laravel-logolockup-cmyk-red.svg" width="400"></p>
 
 <p align="center">
@@ -6,14 +21,6 @@
 <a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/v/stable.svg" alt="Latest Stable Version"></a>
 <a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/license.svg" alt="License"></a>
 </p>
-
-# Generic Social Media Platform
-
-Attempting to make a "Social Media" 
-First time using Laravel, mostly experimenting
-
-
-
 
 
 ## About Laravel
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>SoMe</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -23,8 +23,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div><img class="mx-1 my-auto"  src="https://cdn4.iconfinder.com/data/icons/arrow-68/48/87-512.png" width="40px"/></div>
+                    <div class="my-auto">GenericSocialMediaPlatform</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
